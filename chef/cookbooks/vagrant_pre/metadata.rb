793f5a98ee9bf3727ee47dfa99123afa7a341b53
Jan Van Sweevelt <vansweej@gmail.com>
2014-08-22 22:11:58 +0200

    no chef

--- chef/cookbooks/vagrant_pre/metadata.rb
+++
@@ -1,7 +0,0 @@
-name             'vagrant_pre'
-maintainer       ''
-maintainer_email 'vansweej@gmail.com'
-license          'Apache License'
-description      'Installs/Configures vagrant setup to test dotfiles'
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.0'
--- install-deps.sh
+++ install-deps.sh
@@ -1,4 +1,11 @@
-
+sudo apt-get -y install curl
+sudo apt-get -y install xsel
+sudo apt-get -y install vim
+sudo apt-get -y install tmux
+sudo apt-get -y install fonts-inconsolata
+sudo apt-get -y install irssi
+sudo apt-get -y install git
+sudo apt-get -y install rxvt-unicode
 
 # https://rvm.io
 # rvm for the rubiess
